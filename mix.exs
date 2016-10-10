9a86df031566ff97bc0bbe0aed90c65185405f60
Sam Schneider <sschneider@overdrive.com>
2016-10-10 19:26:29 -0400

    fix up mix.exs for hex:

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule ElasticsearchLoggerBackend.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package(),
+     description: description()]
   end
 
   # Configuration for the OTP application
@@ -15,6 +17,22 @@ defmodule ElasticsearchLoggerBackend.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :timex, :hackney, :poison]]
+  end
+
+  def description do
+    """
+    Send logs in batches via the elasticsearch bulk index api!
+    """
+  end
+
+  def package do
+    [# These are the default files included in the package
+     name: :elasticsearch_logger_backend,
+     files: ["lib", "mix.exs", "README*", "LICENSE*",],
+     maintainers: ["Sam Schneider"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/sschneider1207/elasticsearch_logger_backend/",
+      "Docs" => "https://hexdocs.pm/elasticsearch_logger_backend/"}]
   end
 
   # Dependencies can be Hex packages:
@@ -29,6 +47,7 @@ defmodule ElasticsearchLoggerBackend.Mixfile do
   defp deps do
     [{:timex, "~> 3.0"},
      {:hackney, "~> 1.6"},
-     {:poison, "~> 3.0"}]
+     {:poison, "~> 3.0"},
+     {:ex_doc, "~> 0.14.2", only: :dev}]
   end
 end
